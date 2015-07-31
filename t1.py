a980a48aaabfb39cdd94261a691818337a027074
root <root@cobbler.chinacloud.com.cn>
2015-07-31 14:58:56 +0800

    x

--- t1.py
+++ t1.py
@@ -5,7 +5,7 @@ import re
 try:
     with open('/etc/hosts','r') as f:
         for line in f.readlines():
-            m = re.match(r'(.*)cobbler(.*)',line,re.I)
+            m = re.search(r'cobbler',line,re.I)
             if m:
                 print line.split()[0]
             else:
