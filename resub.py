69feb3af457b39fc6b2bf5066afba003f0d91366
toby82 <44012573@qq.com>
2015-07-31 15:26:17 +0800

    ##test commit##

--- resub.py
+++ resub.py
@@ -2,6 +2,6 @@
 #coding=utf-8
 __author__ = 'Administrator'
 import re
-teststr = "this is test ! # Halllllllllllll lsllsll"
+teststr = "this is test中文 ! # Halllllllllllll lsllsll"
 nn = re.sub(r'#.*$',"",teststr)
 print(nn)
