651429af32ffef372135755a47ae41fbca9a2216
toby82 <44012573@qq.com>
2015-07-31 17:21:17 +0800

    ##test commit##

--- resub.py
+++ resub.py
@@ -1,7 +1,13 @@
 #/usr/bin/python
 #coding=utf-8
+"""
+this is test doc!
+"""
 __author__ = 'Administrator'
 import re
-teststr = "this is test中文 ! # Halllllllllllll lsllsll"
-nn = re.sub(r'#.*$',"",teststr)
-print(nn)
+with open('/etc/hosts','r') as f:
+    for line in f.readlines():
+        teststr = "this is test 中文！# Halllllllllllll lsllsll"
+        nn = re.sub(r'cn.cn',"com",line)
+        print(nn)
+        #print(__doc__)
